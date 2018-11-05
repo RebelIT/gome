9cad8a53703f2baa5cdb4956b80baa7f1b9268ef
Andrew Novak <andrew.novak@target.com>
2018-11-04 23:28:34 -0600

    split cache out as it is being used more

--- cache/cache.go
+++ cache/cache.go
@@ -1,10 +1,29 @@
-package runner
+package cache
 
 import (
 	"fmt"
 	"github.com/gomodule/redigo/redis"
 )
 
+type Devices struct{
+	Device		string `json:"device"`
+	Name		string `json:"name"`
+	Addr 		string `json:"address"`
+	NetPort		string `json:"port"`
+	Id			string `json:"id"`
+	Key 		string `json:"key"`
+}
+
+type Inputs struct{
+	Database	string `json:"database"`
+	Devices		[]Devices
+}
+
+type Status struct{
+	Device	string `json:"device"`
+	Alive	bool `json:"alive"`
+	Url		string `json:"url"`
+}
 
 func CacheSetHash(db string, args redis.Args) error {
 	c, err := redis.Dial("tcp", db)
@@ -34,6 +53,24 @@ func CacheSet(db string, key string, value string) error {
 
 func CacheGetHash(db string, key string) (Devices, error)  {
 	s := Devices{}
+	c, err := redis.Dial("tcp", db)
+	if err != nil {
+		return s, err
+	}
+	defer c.Close()
+
+	values, err := redis.Values(c.Do("HGETALL", key))
+	if err != nil {
+		return s, err
+	}
+
+	redis.ScanStruct(values, &s)
+
+	return s, nil
+}
+
+func GetStatus(db string, key string) (Status, error)  {
+	s := Status{}
 	c, err := redis.Dial("tcp", db)
 	if err != nil {
 		return s, err
