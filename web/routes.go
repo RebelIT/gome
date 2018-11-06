b431c6354471f6ca01e897a01c9f251d84775632
Andrew Novak <andrew.novak@target.com>
2018-11-05 22:11:07 -0600

    do the roku things

--- web/routes.go
+++ web/routes.go
@@ -2,6 +2,7 @@ package listener
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/rebelit/gome/devices/roku"
 	"github.com/rebelit/gome/devices/rpi"
 	"net/http"
 )
@@ -30,9 +31,15 @@ func NewRouter() *mux.Router {
 }
 
 var routes = Routes{
+	//Core
 	Route{"coreDevice", "GET", "/api/device/all", getDevices},
 	Route{"coreDevice", "POST", "/api/device", addDevice},
+	//RaspberryPi
 	Route{"rpi", "GET", "/api/{device}/details", rpi.HandleDetails},
 	Route{"rpi", "GET", "/api/{device}/status", rpi.HandleStatus},
 	Route{"rpi", "POST", "/api/{device}/action", rpi.DeviceControl},
+	//Roku
+	Route{"roku", "GET", "/api/{device}/details", roku.HandleDetails},
+	Route{"roku", "GET", "/api/{device}/status", roku.HandleStatus},
+	Route{"roku", "POST", "/api/{device}/action", roku.DeviceControl},
 }
