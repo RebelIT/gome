9f10b97c2eaf09e359fe68e4859502d050744451
Andrew Novak <andrew.novak@target.com>
2018-11-05 21:37:05 -0600

    function rename

--- runner/deviceRunner.go
+++ runner/deviceRunner.go
@@ -63,43 +63,7 @@ func DeviceLoader(db string, in Inputs) {
 	//Load Devices into database from startup json
 	for _, d := range in.Devices {
 		fmt.Println("[DEBUG] Adding: " + d.Name)
-		cache.CacheSetHash(db, redis.Args{d.Name}.AddFlat(d))
+		cache.SetHash(db, redis.Args{d.Name}.AddFlat(d))
 	}
 	return
 }
-
-
-//func inventoryDevices() (*Inputs, error) {
-//	i := Inputs{}
-//
-//	deviceFile, err := ioutil.ReadFile(FILE)
-//	if err != nil {
-//		fmt.Println(err)
-//		return &i, err
-//	}
-//	fmt.Println("[DEBUG] Loaded json")
-//
-//	json.Unmarshal(deviceFile, &i)
-//	return &i, nil
-//}
-//
-//func StatusRunner(db string, in Inputs) {
-//	fmt.Println("[DEBUG] Starting Status Runners")
-//	for _, d := range in.Devices {
-//		info, err := CacheGetHash(db, "device_"+d.Name)
-//		if err != nil {
-//			fmt.Println(err)
-//		}
-//
-//		d := net.Dialer{Timeout: time.Millisecond * 50}
-//		conn, err := d.Dial("tcp", info.Addr+":"+info.NetPort)
-//		if err != nil {
-//			fmt.Println(info.Name + " is not available on " + info.NetPort)
-//			CacheSet(db, "status_"+info.Name, "false")
-//		} else {
-//			defer conn.Close()
-//			fmt.Println(info.Name + " is available on " + info.NetPort)
-//			CacheSet(db, "status_"+info.Name, "true")
-//		}
-//	}
-//}
