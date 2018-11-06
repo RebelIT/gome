755677d6e13f48000402f904004c299578642587
Andrew Novak <andrew.novak@target.com>
2018-11-05 22:14:40 -0600

    structs...f-ing xml

--- devices/roku/structs.go
+++ devices/roku/structs.go
@@ -1,1 +1,26 @@
 package roku
+
+type Devices struct{
+	Device		string `json:"device"`
+	Name		string `json:"name"`
+	Addr 		string `json:"address"`
+	NetPort		string `json:"port"`
+	Id			string `json:"id"`
+	Key 		string `json:"key"`
+}
+
+//XML :facepalm
+type App struct {
+	App		string `xml:",chardata"`
+	Id		string  `xml:"id,attr"`
+}
+
+type Apps struct {
+	Apps	[]App		`xml:"app"`
+}
+
+type DeviceAction struct{
+	DeviceName	string `json:"device_name"`
+	Action		string `json:"action"`
+	ActionItem	string `json:"action_item"`
+}
