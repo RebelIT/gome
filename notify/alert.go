5dbdba29b876ae545dfac830c7703c253bcd1217
Andrew Novak <andrew.novak@target.com>
2018-12-01 17:18:40 -0600

    notify slack function in new package

--- notify/alert.go
+++ notify/alert.go
@@ -1,1 +1,27 @@
 package notify
+
+import (
+	"fmt"
+	"github.com/rebelit/gome/common"
+)
+
+const FILE  = "./secrets.json"
+
+func SendSlackAlert (message string){
+	s, err := getSecrets()
+	if err != nil{
+		fmt.Printf("[ERROR] slack alert: %s\n", err)
+	}
+
+	content := SlackMsg{}
+	content.Text = message
+	content.Username = "gome"
+
+	respCode, err := common.PostWebReq(content, "https://hooks.slack.com/services/"+ s.SlackSecret)
+	if err != nil{
+		fmt.Printf("[ERROR] slack alert: %s\n", err)
+	}
+	if respCode != 200 {
+		fmt.Printf("[ERROR] slack alert: %s\n", fmt.Errorf("slack returned a non 200 response"))
+	}
+}
