ec5c97095bde44423486f354b9d259303894afa4
Andrew Novak <andrew.novak@target.com>
2018-12-08 22:29:39 -0600

    new metric fucnctions

--- notify/metric.go
+++ notify/metric.go
@@ -3,21 +3,20 @@ package notify
 import (
 	"github.com/rebelit/gome/common"
 	"gopkg.in/alexcesaro/statsd.v2"
-	"log"
 	"strconv"
 )
 
 
 func MetricDeviceStatus(deviceName string, deviceType string, alive bool){
-	//emits a new counter for every external web request
+	//emits a new generic metric for device status
 	tags := statsd.Tags("device", deviceName, "device_type", deviceType)
 	measurement := "gome_device"
 	value := ""
 
 	if alive{
-		value = "1"
+		value = "on"
 	} else{
-		value = "0"
+		value = "off"
 	}
 
 	sendUnique(measurement, tags, value)
@@ -25,7 +24,7 @@ func MetricDeviceStatus(deviceName string, deviceType string, alive bool){
 
 func MetricHttpIn(uri string, reponseCode int, method string){
 	//emits a new counter for every incoming http web request
-	tags := statsd.Tags("uri", uri, "status_code", strconv.Itoa(reponseCode))
+	tags := statsd.Tags("uri", uri, "status_code", strconv.Itoa(reponseCode),"method", method)
 	measurement := "gome_http"
 
 	sendCounter(measurement, tags)
@@ -39,12 +38,24 @@ func MetricHttpOut(uri string, reponseCode int, method string){
 	sendCounter(measurement, tags)
 }
 
+func MetricCmd(cmd string, response string){
+	//emits a new counter for every shell out function
+	tags := statsd.Tags("cmd", cmd, "cmd_status", response)
+	measurement := "gome_cmd"
+
+	sendCounter(measurement, tags)
+}
+
 func sendCounter(measurement string, tags statsd.Option){
+	if common.STATSD == ""{
+		//metrics disabled don't do anything
+		return
+	}
 	addrOpt := statsd.Address(common.STATSD)
 	fmtOpt := statsd.TagsFormat(statsd.InfluxDB)
 	c, err := statsd.New(addrOpt,fmtOpt,tags)
 	if err != nil {
-		log.Print(err)
+		//log.Print(err)
 	}
 	defer c.Close()
 
@@ -52,11 +63,15 @@ func sendCounter(measurement string, tags statsd.Option){
 }
 
 func sendUnique(measurement string, tags statsd.Option, value string){
+	if common.STATSD == ""{
+		//metrics disabled don't do anything
+		return
+	}
 	addrOpt := statsd.Address(common.STATSD)
 	fmtOpt := statsd.TagsFormat(statsd.InfluxDB)
 	c, err := statsd.New(addrOpt,fmtOpt,tags)
 	if err != nil {
-		log.Print(err)
+		//log.Print(err)
 	}
 	defer c.Close()
 
