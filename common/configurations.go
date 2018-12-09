391baf904b62760594789334c3ca1431bd09aa0b
Andrew Novak <andrew.novak@target.com>
2018-12-08 21:34:47 -0600

    statsd metrics

--- common/configurations.go
+++ common/configurations.go
@@ -3,6 +3,7 @@ package common
 // flip this to use when deployed
 const FILE  = "/etc/gome/devices.json"
 const SECRETS = "/etc/gome/secrets.json"
+const STATSD = "127.0.0.1:8125"
 
 // flip this on for local testing
 //const FILE  = "./devices.json"
