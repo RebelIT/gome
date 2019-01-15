cd62579487ccfaa06fb628489d6dd3f1b7c46982
Andrew Novak <andrew.novak@target.com>
2019-01-14 22:49:50 -0600

    flip to production use configs

--- common/configurations.go
+++ common/configurations.go
@@ -1,11 +1,11 @@
 package common
 
 // flip this to use when deployed
-//const FILE  = "/etc/gome/devices.json"
-//const SECRETS = "/etc/gome/secrets.json"
+const FILE  = "/etc/gome/devices.json"
+const SECRETS = "/etc/gome/secrets.json"
 //const STATSD = "127.0.0.1:8125"     // set to empty string to disable metrics
 
 // flip this on for local testing
-const FILE  = "./devices.json"
-const SECRETS = "/.secrets.json"
+//const FILE  = "./devices.json"
+//const SECRETS = "./secrets.json"
 const STATSD = ""
