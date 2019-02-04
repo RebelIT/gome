0613793f801dfff6b0647ee0d6e8377db3aabb56
Andrew Novak <andrew.novak@target.com>
2019-02-03 22:37:23 -0600

    added common http methods

--- common/configurations.go
+++ common/configurations.go
@@ -7,6 +7,7 @@ const STATSD = "127.0.0.1:8125"     // set to empty string to disable metrics
 const SCHEDULE_MIN = 1
 const STATUS_MIN = 5
 const AWS_SEC = 2
+const HTTP_TIMEOUT = 2
 
 // flip this on for local testing
 //const FILE  = "./devices.json"
--- common/http.go
+++ common/http.go
@@ -1,23 +1,101 @@
 package common
 
 import (
-	"bytes"
+	"context"
 	"encoding/json"
 	"net/http"
+	"time"
 )
 
-func PostWebReq (reqBody interface{}, reqUrl string) (respCode int, respError error){
-	contentType := "application/json"
-	body, err := json.Marshal(reqBody)
+//NOTE:
+//	Project uses json all request body gets marshaled into json
+//
 
+func HttpPost(url string, body interface{}, headers map[string]string)(response http.Response, err error){
+	ctx, cncl := context.WithTimeout(context.Background(), time.Second * HTTP_TIMEOUT)
+	defer cncl()
+
+	req, err := http.NewRequest(http.MethodPost, url, nil)
 	if err != nil{
-		return 0, err
+		return http.Response{}, err
 	}
 
-	resp, err := http.Post(reqUrl, contentType, bytes.NewReader(body))
-	if err != nil{
-		return 0, err
+	b, _ := json.Marshal(body)
+	req.Body.Read(b)
+
+	for key, value := range headers {
+		req.Header.Set(key,value)
 	}
 
-	return resp.StatusCode, nil
+	resp, err := http.DefaultClient.Do(req.WithContext(ctx))
+	if err != nil{
+		return http.Response{}, err
+	}
+
+	return *resp, nil
+}
+
+func HttpPut(url string, body interface{}, headers map[string]string)(response http.Response, err error){
+	ctx, cncl := context.WithTimeout(context.Background(), time.Second * HTTP_TIMEOUT)
+	defer cncl()
+
+	req, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil{
+		return http.Response{}, err
+	}
+
+	b, _ := json.Marshal(body)
+	req.Body.Read(b)
+
+	for key, value := range headers {
+		req.Header.Set(key,value)
+	}
+
+	resp, err := http.DefaultClient.Do(req.WithContext(ctx))
+	if err != nil{
+		return http.Response{}, err
+	}
+
+	return *resp, nil
+}
+
+func HttpDelete(url string, body interface{}, headers map[string]string)(response http.Response, err error){
+	ctx, cncl := context.WithTimeout(context.Background(), time.Second * HTTP_TIMEOUT)
+	defer cncl()
+
+	req, err := http.NewRequest(http.MethodDelete, url, nil)
+	if err != nil{
+		return http.Response{}, err
+	}
+
+	b, _ := json.Marshal(body)
+	req.Body.Read(b)
+
+	for key, value := range headers {
+		req.Header.Set(key,value)
+	}
+
+	resp, err := http.DefaultClient.Do(req.WithContext(ctx))
+	if err != nil{
+		return http.Response{}, err
+	}
+
+	return *resp, nil
+}
+
+func HttpGet(url string)(response http.Response, error error){
+	ctx, cncl := context.WithTimeout(context.Background(), time.Second * HTTP_TIMEOUT)
+	defer cncl()
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil{
+		return http.Response{}, err
+	}
+
+	resp, err := http.DefaultClient.Do(req.WithContext(ctx))
+	if err != nil{
+		return http.Response{}, err
+	}
+
+	return *resp, nil
 }
