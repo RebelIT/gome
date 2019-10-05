78c887d316c884fa6a4046387cb3e3b30ae60592
RebelIT <31414479+RebelIT@users.noreply.github.com>
2019-10-04 23:59:04 -0500

    new database connector

---
+++ database/badger.go
@@ -0,0 +1,67 @@
+package database
+
+import (
+	"fmt"
+	"github.com/rebelit/gome/common"
+	db "github.com/rebelit/gome_badger"
+
+)
+
+//type Profile struct {
+//	path string
+//	name string
+//}
+
+func Get(name string) (data string, error error) {
+	conn, err := db.Open(common.DBPATH)
+	if err != nil {
+		return "", fmt.Errorf("open database: %s", err)
+	}
+	defer conn.Close()
+
+	value, err := conn.Get(name)
+	if err != nil {
+		return "", fmt.Errorf("get database value: %s", err)
+	}
+	return value, nil
+}
+
+func Add(key, value string) error {
+	conn, err := db.Open(common.DBPATH)
+	if err != nil {
+		return fmt.Errorf("open database: %s", err)
+	}
+	defer conn.Close()
+
+	if err := conn.Set(key, value); err != nil {
+		return fmt.Errorf("add database value: %s", err)
+	}
+	return nil
+}
+
+func Del(key string) error {
+	conn, err := db.Open(common.DBPATH)
+	if err != nil {
+		return fmt.Errorf("open database: %s", err)
+	}
+	defer conn.Close()
+
+	if err := conn.Delete(key); err != nil {
+		return fmt.Errorf("del database key: %s", err)
+	}
+	return nil
+}
+
+func GetAll() (data []string, error error) {
+	conn, err := db.Open(common.DBPATH)
+	if err != nil {
+		return nil, fmt.Errorf("open database: %s", err)
+	}
+	defer conn.Close()
+
+	value, err := conn.GetAllKeys()
+	if err != nil {
+		return nil, fmt.Errorf("get all database values: %s", err)
+	}
+	return value, nil
+}
